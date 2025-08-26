069e31c57bce6a22c2e228d8f4aaba0782f7b3f5
GamzeCindik <gamze.cindik@bites.com.tr>
2025-08-26 11:42:14 +0300

    Life Planner projesi ilk git bağlantısı yapıldı.

---
+++ backend/life-planner/src/main/java/com/example/life_planner/LifePlannerApplication.java
@@ -0,0 +1,14 @@
+package com.example.life_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class LifePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifePlannerApplication.class, args);
+	}
+
+}
---
+++ backend/life-planner/src/test/java/com/example/life_planner/LifePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.life_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
